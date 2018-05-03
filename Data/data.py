92016f6a9353684e4c5f10eb59213abae7785c78
kvour <32718783+kvour@users.noreply.github.com>
2018-05-03 05:21:17 -0400

    Add files via upload

--- Data/data.py
+++ Data/data.py
@@ -41,7 +41,7 @@ class CustomDataset(Dataset):
             sample = self.transform(sample)
 
         return sample
-    
+
 class Rescale(object):
     """Rescale the image in a sample to a given size.
 
@@ -102,10 +102,10 @@ class CenterCrop(object):
 
         #top = np.random.randint(0, h - new_h)
         #left = np.random.randint(0, w - new_w)
-        
+
         top = (h - new_h)//2
         left = (w - new_w)//2
-        
+
         image = image[top: top + new_h,
                       left: left + new_w]
 
@@ -128,30 +128,168 @@ class ToTensor(object):
                 'label': torch.FloatTensor([label[0].tolist()])}
 
 
-transformed_dataset_ccw = CustomDataset(csv_file='~/Documents/AML_Project/Data/Manual_updated/man_ccw.csv',
-                                           root_dir='./Data/Manual_updated/CCW/',
+if args.model=='Alexnet':
+    size = 224
+else:
+    size = 299
+
+
+transformed_dataset_cw_1 = CustomDataset(csv_file='./Data/new_man/im1_labels.csv',
+                                           root_dir='./Data/new_man/images1/',
                                            transform=transforms.Compose([
-                                               Rescale(299),
-                                               CenterCrop(299),
+                                               Rescale(size),
+                                               CenterCrop(size),
                                                ToTensor()
                                            ]))
 
-transformed_dataset_cw = CustomDataset(csv_file='~/Documents/AML_Project/Data/Manual_updated/man_cw.csv',
-                                           root_dir='./Data/Manual_updated/CW/',
+transformed_dataset_cw_2 = CustomDataset(csv_file='./Data/new_man/im2_labels.csv',
+                                           root_dir='./Data/new_man/images2/',
                                            transform=transforms.Compose([
-                                               Rescale(299),
-                                               CenterCrop(299),
+                                               Rescale(size),
+                                               CenterCrop(size),
                                                ToTensor()
                                            ]))
 
-transformed_dataset = ConcatDataset( [transformed_dataset_cw, transformed_dataset_ccw] )
+transformed_dataset_cw_3 = CustomDataset(csv_file='./Data/new_man/im3_labels.csv',
+                                           root_dir='./Data/new_man/images3/',
+                                           transform=transforms.Compose([
+                                               Rescale(size),
+                                               CenterCrop(size),
+                                               ToTensor()
+                                           ]))
+
+transformed_dataset_cw_4 = CustomDataset(csv_file='./Data/new_man/im4_labels.csv',
+                                           root_dir='./Data/new_man/images4/',
+                                           transform=transforms.Compose([
+                                               Rescale(size),
+                                               CenterCrop(size),
+                                               ToTensor()
+                                           ]))
+
+
+transformed_dataset_cw_5 = CustomDataset(csv_file='./Data/new_man/im5_labels.csv',
+                                           root_dir='./Data/new_man/images5/',
+                                           transform=transforms.Compose([
+                                               Rescale(size),
+                                               CenterCrop(size),
+                                               ToTensor()
+                                           ]))
+
+
+transformed_dataset_cw_6 = CustomDataset(csv_file='./Data/new_man/im6_labels.csv',
+                                           root_dir='./Data/new_man/images6/',
+                                           transform=transforms.Compose([
+                                               Rescale(size),
+                                               CenterCrop(size),
+                                               ToTensor()
+                                           ]))
+
+transformed_dataset_cw_7 = CustomDataset(csv_file='./Data/Manual_updated/man_ccw.csv',
+                                           root_dir='./Data/Manual_updated/CCW_updt/',
+                                           transform=transforms.Compose([
+                                               Rescale(size),
+                                               CenterCrop(size),
+                                               ToTensor()
+                                           ]))
+
+transformed_dataset_cw_8 = CustomDataset(csv_file='./Data/Manual_updated/new_dataset/man1_labels.csv',
+                                           root_dir='./Data/Manual_updated/new_dataset/man1_res/',
+                                           transform=transforms.Compose([
+                                               Rescale(size),
+                                               CenterCrop(size),
+                                               ToTensor()
+                                           ]))
+transformed_dataset_cw_9 = CustomDataset(csv_file='./Data/Manual_updated/new_dataset/man2_labels.csv',
+                                           root_dir='./Data/Manual_updated/new_dataset/man2_res/',
+                                           transform=transforms.Compose([
+                                               Rescale(size),
+                                               CenterCrop(size),
+                                               ToTensor()
+                                           ]))
+
+transformed_dataset_cw_10 = CustomDataset(csv_file='./Data/Manual_updated/new_dataset/cw_comp1.csv',
+                                           root_dir='./Data/Manual_updated/new_dataset/cw_1_resize/',
+                                           transform=transforms.Compose([
+                                               Rescale(size),
+                                               CenterCrop(size),
+                                               ToTensor()
+                                           ]))
+
+transformed_dataset_cw_11 = CustomDataset(csv_file='./Data/Manual_updated/new_dataset/cw_comp2.csv',
+                                           root_dir='./Data/Manual_updated/new_dataset/cw_2_resize/',
+                                           transform=transforms.Compose([
+                                               Rescale(size),
+                                               CenterCrop(size),
+                                               ToTensor()
+                                           ]))
+
+
+
+if args.aug == 1:
+  transformed_dataset_cw_1_aug = CustomDataset(csv_file='./Data/new_man/im1_labels.csv',
+                                             root_dir='./Data/new_man/images1_aug/',
+                                             transform=transforms.Compose([
+                                                 Rescale(size),
+                                                 CenterCrop(size),
+                                                 ToTensor()
+                                             ]))
+
+  transformed_dataset_cw_2_aug = CustomDataset(csv_file='./Data/new_man/im2_labels.csv',
+                                             root_dir='./Data/new_man/images2_aug/',
+                                             transform=transforms.Compose([
+                                                 Rescale(size),
+                                                 CenterCrop(size),
+                                                 ToTensor()
+                                             ]))
+
+  transformed_dataset_cw_3_aug = CustomDataset(csv_file='./Data/new_man/im3_labels.csv',
+                                             root_dir='./Data/new_man/images3_aug/',
+                                             transform=transforms.Compose([
+                                                 Rescale(size),
+                                                 CenterCrop(size),
+                                                 ToTensor()
+                                             ]))
+
+  transformed_dataset_cw_4_aug = CustomDataset(csv_file='./Data/new_man/im4_labels.csv',
+                                             root_dir='./Data/new_man/images4_aug/',
+                                             transform=transforms.Compose([
+                                                 Rescale(size),
+                                                 CenterCrop(size),
+                                                 ToTensor()
+                                             ]))
+
+
+  transformed_dataset_cw_5_aug = CustomDataset(csv_file='./Data/new_man/im5_labels.csv',
+                                             root_dir='./Data/new_man/images5_aug/',
+                                             transform=transforms.Compose([
+                                                 Rescale(size),
+                                                 CenterCrop(size),
+                                                 ToTensor()
+                                             ]))
+
+  transformed_dataset_cw_6_aug = CustomDataset(csv_file='./Data/new_man/im6_labels.csv',
+                                             root_dir='./Data/new_man/images6_aug/',
+                                             transform=transforms.Compose([
+                                                 Rescale(size),
+                                                 CenterCrop(size),
+                                                 ToTensor()
+                                             ]))
+
+
+
+if args.aug == 1:
+  transformed_dataset = ConcatDataset( [transformed_dataset_cw_1, transformed_dataset_cw_2, transformed_dataset_cw_3, transformed_dataset_cw_4, transformed_dataset_cw_5, transformed_dataset_cw_6,
+    transformed_dataset_cw_1_aug, transformed_dataset_cw_2_aug, transformed_dataset_cw_3_aug, transformed_dataset_cw_4_aug, transformed_dataset_cw_5_aug, transformed_dataset_cw_6_aug ] )
+
+else:
+  transformed_dataset = ConcatDataset( [transformed_dataset_cw_1, transformed_dataset_cw_2, transformed_dataset_cw_3, transformed_dataset_cw_4, transformed_dataset_cw_5, transformed_dataset_cw_6, transformed_dataset_cw_7, transformed_dataset_cw_8, transformed_dataset_cw_9, transformed_dataset_cw_10, transformed_dataset_cw_11 ] )
 
 train_len = np.int( args.train_percent * len(transformed_dataset) )
 
 train_data , test_data = random_split( transformed_dataset , [train_len , len(transformed_dataset)-train_len ] )
 
 # load trainig data loader
-kwargs = {'num_workers': 4, 'pin_memory': True} if args.cuda else {}
+kwargs = {'num_workers': 8, 'pin_memory': True} if args.cuda else {}
 train_loader = DataLoader(train_data,
                           batch_size=args.batch_size, shuffle=True, drop_last=False, **kwargs)
 
